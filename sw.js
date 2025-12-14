81d9c32dcf44012850de3b150137a7a5e9cc4c23
lucidpp <itslucidpp@gmail.com>
2025-12-14 16:03:26 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -7,3 +7,13 @@ self.addEventListener("fetch", (event) => {
     fetch(event.request).catch(() => new Response("Offline"))
   );
 });
+
+<script>
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js')
+      .then(reg => console.log('Service Worker registered:', reg))
+      .catch(err => console.log('Service Worker registration failed:', err));
+  });
+}
+</script>
